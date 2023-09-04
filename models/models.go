7405366650607ffc961015c545bf31111924b784
Otaviano Almeida <otaviano@itstransdata.com>
2023-09-04 18:29:16 -0300

    Better defines structs used in REST endpoints

--- models/models.go
+++ models/models.go
@@ -7,4 +7,13 @@ type Client struct {
 	Tel string `json:"tel" example:"19970675070"`
 	Zipcode string `json:"zipcode" example:"13024430"`
 	Address string `json:"address" example:"R. Dr. Sampaio Ferraz, 216"`
-}	
+}
+
+type ClientCreateQuery struct {
+	Name string `json:"name" example:"João da Silva"`
+	Tel string `json:"tel" example:"19970675070"`
+	Zipcode string `json:"zipcode" example:"13024430"`
+	Address string `json:"address" example:"R. Dr. Sampaio Ferraz, 216"`
+}
+
+type ClientUpdateQuery ClientCreateQuery
--- server/controllers/clients.go
+++ server/controllers/clients.go
@@ -19,7 +19,7 @@ import (
 // @Tags Clients v1
 // @Accept json
 // @Produce json
-// @Param	client	body	models.Client	true	"Client to add"
+// @Param	client	body	models.ClientCreateQuery	true	"Client to add"
 // @Success 200 {object} models.Client
 // @Failure 415
 // @Failure 500 {object} ResultError
@@ -55,8 +55,15 @@ func createClient(ctx *fiber.Ctx) (err error) {
 		return
 	}
 
-	toCreateClient := models.Client{}
-	ffjson.Unmarshal(ctx.Body(), &toCreateClient)
+	toCreateClientQuery := models.ClientCreateQuery{}
+	ffjson.Unmarshal(ctx.Body(), &toCreateClientQuery)
+
+	toCreateClient := models.Client{
+		Name: toCreateClientQuery.Name,
+		Tel: toCreateClientQuery.Tel,
+		Zipcode: toCreateClientQuery.Zipcode,
+		Address: toCreateClientQuery.Address,
+	}
 
 	createdClient, err := database.CreateClient(toCreateClient)
 	if err != nil {
@@ -211,7 +218,7 @@ func getClients(ctx *fiber.Ctx) (err error) {
 // @Accept json
 // @Produce json
 // @Param	id	path	string	true	"Client ID"
-// @Param	client	body	models.Client	true	"Desired client"
+// @Param	client	body	models.ClientUpdateQuery	true	"Desired client"
 // @Success 200 {object} models.Client
 // @Success 400 {object} ResultError
 // @failure 404 {object} ResultError
@@ -266,8 +273,15 @@ func replaceClient(ctx *fiber.Ctx) (err error) {
 
 	}
 
-	desiredClient := models.Client{}
-	ffjson.Unmarshal(ctx.Body(), &desiredClient)
+	desiredClientQuery := models.ClientUpdateQuery{}
+	ffjson.Unmarshal(ctx.Body(), &desiredClientQuery)
+
+	desiredClient := models.Client{
+		Name: desiredClientQuery.Name,
+		Tel: desiredClientQuery.Tel,
+		Zipcode: desiredClientQuery.Zipcode,
+		Address: desiredClientQuery.Address,
+	}
 
 	updatedClient, err := database.UpdateClient(parsedclientIDToUpdate.String(), desiredClient)
 	if err != nil {
@@ -300,7 +314,7 @@ func replaceClient(ctx *fiber.Ctx) (err error) {
 // @Accept json
 // @Produce json
 // @Param	id	path	string	true	"Client ID"
-// @Param	client	body	models.Client	true	"Client fields to update"
+// @Param	client	body	models.ClientUpdateQuery	true	"Client fields to update"
 // @Success 200 {object} models.Client
 // @Failure 400 {object} ResultError
 // @Failure 404 {object} ResultError
@@ -370,8 +384,15 @@ func updateClient(ctx *fiber.Ctx) (err error) {
 		return
 	}
 
-	desiredClient := models.Client{}
-	ffjson.Unmarshal(ctx.Body(), &desiredClient)
+	desiredClientQuery := models.ClientUpdateQuery{}
+	ffjson.Unmarshal(ctx.Body(), &desiredClientQuery)
+
+	desiredClient := models.Client{
+		Name: desiredClientQuery.Name,
+		Tel: desiredClientQuery.Tel,
+		Zipcode: desiredClientQuery.Zipcode,
+		Address: desiredClientQuery.Address,
+	}
 
 	// updates retrievedClient with desiredClient's non-zero fields
 	v := reflect.ValueOf(models.Client{})
